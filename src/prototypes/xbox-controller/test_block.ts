43b2ccd6c155f3bba76950e204a7f346c362492f
AIDDEI <aidendeightonschool@gmail.com>
2022-06-23 16:00:17 +0200

    Xbox Controls

---
+++ src/prototypes/xbox-controller/test_block.ts
@@ -0,0 +1,17 @@
+// Import PIXI
+import * as PIXI from 'pixi.js';
+
+export class Block extends PIXI.Sprite{
+
+    constructor(texture: PIXI.Texture){
+        super(texture);
+
+        // Setting the start position
+        this.x = 350;
+        this.y = 150;
+
+        // Setting the width & height
+        this.width = 70;
+        this.height = 72;
+    }
+}
